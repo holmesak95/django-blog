93c498a3ae0f1f109c1c8056296f9d2cf971bbed
holmesak95 <holmesak95@gmail.com>
2022-12-05 21:24:50 -0600

    Finished activity and tests

--- blogging/models.py
+++ blogging/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
     published_date = models.DateTimeField(blank=True, null=True)
-    
+
     def __str__(self):
         return self.title
 
@@ -20,6 +20,6 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
-    
+
     class Meta:
         verbose_name_plural = 'Categories'
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,13 @@
 # mysite/urls.py
 
 from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include  # <-- Make sure you have both of these imports.
 
 urlpatterns = [
-    path('polling/', include('polling.urls')),  # <-- Add this
     path('admin/', admin.site.urls),
+    path('polling/', include('polling.urls')),
+    path('', include('blogging.urls')),
+    path('login/', LoginView.as_view(template_name='login.html'), name="login"),
+    path('logout/', LogoutView.as_view(next_page='/'), name="logout"),
 ]
