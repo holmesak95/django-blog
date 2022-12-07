a84f9d63395b09660914e7c5a551b32cbbb37a85
holmesak95 <holmesak95@gmail.com>
2022-12-06 19:07:56 -0600

    Finished assignment

--- blogging/admin.py
+++ blogging/admin.py
@@ -3,5 +3,20 @@ from django.contrib import admin
 from blogging.models import Post, Category
 
 # and a new admin registration
-admin.site.register(Post)
-admin.site.register(Category)
+#admin.site.register(Post)
+#admin.site.register(Category)
+
+# https://docs.djangoproject.com/en/dev/ref/contrib/admin/#working-with-many-to-many-models
+
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CategoryInline,]
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [CategoryInline]
+    exclude = ['posts']
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
