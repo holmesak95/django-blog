52e12329f41168a491821d6c0b7e7e133385035d
holmesak95 <holmesak95@gmail.com>
2022-12-12 21:39:53 -0600

    Finished assignment by using ListView and DetailView from Djangos custom classes

--- blogging/urls.py
+++ blogging/urls.py
@@ -2,9 +2,9 @@
 # Title: urls.py
 # Desc: <Enter Description here>
 #---------------------------------------------------------------------#
-
+# 1:53:57
 from django.urls import path
-from blogging.views import stub_view, list_view, detail_view
+from blogging.views import BloggingListView, BloggingDetailView
 
-urlpatterns = [path('', list_view, name="blog_index"),
-               path('posts/<int:post_id>/', detail_view, name="blog_detail"),]
+urlpatterns = [path('', BloggingListView.as_view(), name="blog_index"),
+               path('posts/<int:post_id>/', BloggingDetailView.as_view(), name="blog_detail"),]
